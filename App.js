2b3e9bc348a817d94d1c32a3d05a55ac4160e591
Ngô Tuấn Anh <venh2210@gmail.com>
2024-02-24 16:40:42 +0700

    folder

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ứng dụng cộng đồng chia sẻ điểm thưởng N4</Text>
+      <Text>Ứng dụng cộng đồng chia sẻ điểm thưởng N4</Text>
       <StatusBar style="auto" />
     </View>
   );
